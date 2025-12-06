b1114c63796342a725e43518937d61edcd54d9d2
BuiTranTien <buitrantien1804@gmail.com>
2025-12-06 12:11:35 +0700

    fix updatebook

--- src/pages/dashboard/editBook/UpdateBook.jsx
+++ src/pages/dashboard/editBook/UpdateBook.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import InputField from '../addBook/InputField'
+import InputField from '../addBook/inputField'
 import SelectField from '../addBook/SelectField'
 import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
